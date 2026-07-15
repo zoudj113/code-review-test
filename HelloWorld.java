c4dccae324ecd1296b05b815fad9c4ddac7b54e5
zoudj113 <zoudj113@foxmail.com>
2026-07-15 17:54:04 +0800

    [feat](demo): 添加 HelloWorld 示例程序

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) {	
 		String str3 = null;
-		System.out.println(str3.toString());
-		String str1 = null;
-		System.out.println(str1.toString());
 		System.out.println("hello world");
-		System.out.println("hello world");
-		String str5 = null;
-		System.out.println(str5.toString());
-		String str6 = null;
-		System.out.println(str6.toString());
-		String str7 = null;
-		System.out.println(str7.toString());
-		String str8 = null;
-		System.out.println(str8.toString());
-    }
 }
