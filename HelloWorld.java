8f23bdb22e8fff446b7186536fb9a32a57a62100
zoudj113 <zoudj113@foxmail.com>
2026-07-15 17:34:43 +0800

    [feat](demo): 添加 HelloWorld 示例程序

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-		String str2 = null;
-		System.out.println(str2.toString());
 		String str3 = null;
 		System.out.println(str3.toString());
 		String str1 = null;
